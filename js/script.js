cb2b21a939ec30754d36e5f9e86aa5f2cc27dfa1
Luis <luisf.infu@gmail.com>
2025-01-22 18:02:48 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,42 +1,95 @@
 import list from './components/list.js'
 
-//-- Gera uma nova palavra para jogar
+
 const newGenerate = document.getElementById("newGenerate");
 
-//-- Carrega a função que grava valores dos inputs no localStorage
-setTimeout(() => {
-  setData();
-}, "100");
+// setTimeout(() => {
+//   setData();
+// }, "100");
 
-//-- Recarregar palavra e dica
-newGenerate.addEventListener("click", () => {
-  location.reload();
-});
+// newGenerate.addEventListener("click", () => {
+//   location.reload();
+// });
 
-//-- Gera elementos de palavra e dicas para o jogo
-const randomElement = list[Math.floor(Math.random() * list.length)];
 
-//-- Inclui os elementos em seus respectivos inputs na homepage
-  function elements(){
-   document.getElementById("word").value = randomElement.palavra;
-   document.getElementById("tip").value = randomElement.dica;
+const randomElement1 = list.palavrasF[Math.floor(Math.random() * list.palavrasF.length)];
+const randomElement2 = list.palavrasM[Math.floor(Math.random() * list.palavrasM.length)];
+const randomElement3 = list.palavrasD[Math.floor(Math.random() * list.palavrasD.length)];
+    
+  function elementsF(){
+   document.getElementById("word").value = randomElement1.palavra;
+   document.getElementById("tip").value = randomElement1.dica;
   } 
- elements();
 
-//-- Iniciar o jogo com as palavras 
+  function elementsM(){
+    document.getElementById("word").value = randomElement2.palavra;
+    document.getElementById("tip").value = randomElement2.dica;
+   }   
+   
+   function elementsD(){
+    document.getElementById("word").value = randomElement3.palavra;
+    document.getElementById("tip").value = randomElement3.dica;
+   } 
+
+
  let initGame = document.getElementById("initGame");
 
-//-- Capturando as palavras dos inputs
- let wr = document.getElementById("word").value; 
- let tp = document.getElementById("tip").value;
 
-//-- Função que grava as palavras no localStorage
- function setData() {
+function loadNull(){
+  let wr = document.getElementById("word").value; 
+  let tp = document.getElementById("tip").value; 
+  if(wr === "" && tp === ""){
+    document.getElementById("word").value = "********" 
+    document.getElementById("tip").value = "********" 
+  }
+}
+loadNull() 
+
+ function setData() { 
+  let wr = document.getElementById("word").value; 
+  let tp = document.getElementById("tip").value; 
+    console.log(wr, tp)
   localStorage.setItem("word", wr);
   localStorage.setItem("tip", tp);
 }
  
-//-- Chamando a rota do jogo
  initGame.addEventListener("click", () => {
-    location = "./html/home.html"
+    let wr = document.getElementById("word").value; 
+    let tp = document.getElementById("tip").value;
+    if(wr === "********" && tp === "********"){
+      alert("Selecione uma dificuldade antes de iniciar!")
+    }else{
+      location = "./html/home.html"
+    }
+    
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const radios = document.querySelectorAll('input[name="fav_language"]');
+
+  radios.forEach(radio => {
+    radio.addEventListener('change', (event) => {
+      const selectedDifficulty = event.target.value;
+      console.log("Dificuldade selecionada:", selectedDifficulty);
+
+ 
+      if (selectedDifficulty === "Facil") {
+        elementsF()
+
+        setTimeout(() => {
+          setData()
+        }, 500);
+       
+      } else if (selectedDifficulty === "Médio") {
+        elementsM()
+        setData()
+      } else if (selectedDifficulty === "Dificil") {
+        elementsD()
+        setData()
+      }
+    });
+  });
 });
